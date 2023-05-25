50677fba8d014a0bbbbb2222b5413176bac8899b
Robert Sehr <robert.sehr@intranda.com>
2023-05-25 10:15:20 +0200

    deploy to jenkins

--- plugin/src/main/java/org/goobi/interfaces/IArchiveManagementAdministrationPlugin.java
+++ plugin/src/main/java/org/goobi/interfaces/IArchiveManagementAdministrationPlugin.java
@@ -6,17 +6,24 @@ import org.goobi.production.plugin.interfaces.IAdministrationPlugin;
 
 public interface IArchiveManagementAdministrationPlugin extends IAdministrationPlugin {
 
+    @Override
     public String getTitle();
-    
+
     public String getDisplayMode();
 
     public void setDisplayMode(String mode);
-    
+
     public List<String> getPossibleDatabases();
-    
+
     public void setSelectedDatabase(String db);
-    
+
     public void loadSelectedDatabase();
-    
+
     public IEadEntry getRootElement();
+
+
+    // used to create a new database
+    public void setFileName(String fileName);
+    public void setDatabaseName(String databaseName);
+    public void createNewDatabase();
 }
